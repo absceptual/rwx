02a80b1e3ead896cd4f32cebe4174139a4b14bb8
abs <absceptual@gmail.com>
2022-12-12 11:24:23 -0600

    Added additional members and functions

--- renderer/renderer.h
+++ renderer/renderer.h
@@ -1,3 +1,12 @@
+/*
+How to 
+*/
+
+
+
+
+
+
 #pragma once
 #include <iostream>
 #include <memory>
@@ -15,38 +24,36 @@
 #pragma comment (lib, "d3dx10.lib")
 
 
-/* Purpose of this renderer class :
-	- Serve as a wrapper for internal Direct3D 11 functions
 
-
-*/
 class Renderer
 {
 public:
-	using vtables_t = std::pair<void**, void**>;
+	using vtable_t  = void**;
 	using present_t = HRESULT(__stdcall*)(IDXGISwapChain*, UINT, UINT);
-
+	present_t o_present{ nullptr };
+	
 	struct Vertex
 	{
 		FLOAT x, y, z; D3DXCOLOR color;
 	};
 
 private:
+	// Direct3D Interfaces
 	ID3D11Device* m_device{ };
 	IDXGISwapChain* m_swapchain{ };
 	ID3D11DeviceContext* m_ctx{ };
-	ID3D11RenderTargetView* m_renderbuffer{ }; // Target for rendering our primtiives
-	ID3D11VertexShader* m_vshader{ }; // Shader responsible for vertices
-	ID3D11PixelShader* m_pshader{ }; // Shader responsible for pixels
-	ID3D11Buffer* m_buffer{ }; // Buffer for vertices
-	ID3D11InputLayout* m_layout{ }; // Defines vertex layout in memory
-	Renderer::vtables_t m_vtables{};
+	ID3D11RenderTargetView* m_target{ };
+	ID3D11VertexShader* m_vshader{ }; 
+	ID3D11PixelShader* m_pshader{ }; 
+	ID3D11Buffer* m_buffer{ };
+	ID3D11InputLayout* m_layout{ }; 
 
+	// Miscelleanous
 	const wchar_t* m_shader{ L"shader.sco" };
 	bool m_initalized{ false };
 
 public:
-	Renderer() = default;
+	Renderer(const wchar_t* process, present_t hk_present);
 
 	// Meant to be called within a present hook (add compiling shaders)
 	auto init(IDXGISwapChain* device) -> bool;
